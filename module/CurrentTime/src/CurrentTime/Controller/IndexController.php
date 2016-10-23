bb5ea36ed1520f42e37107c0ab60b8c1c9c6e41f
marinazacon <marinazacon@gmail.com>
2016-10-23 12:28:25 +0200

    Creating controllers and views in a new Zend Framework module

---
+++ module/CurrentTime/src/CurrentTime/Controller/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+namespace CurrentTime\Controller;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        $view = new ViewModel();
+        return $view;
+    }
+}
